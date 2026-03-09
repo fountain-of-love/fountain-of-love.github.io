6f9be0f90f62ccb859d335836954fe4f20a639eb
paronella <97030309+paronella@users.noreply.github.com>
2026-03-09 15:28:16 +0100

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function getParams() {
   const lang = SUPPORTED_LANGS.includes(langParam) ? langParam : "en";
   return {
     lang,
-    variant: url.searchParams.get("variant") || "you",
+    variant: url.searchParams.get("variant") || "FoL",
     slug: url.searchParams.get("slug") || "stance-landing"
   };
 }
